e7c843e96abe081dfac4dbc80c21b0704068de24
Nicolas Tournaud <ntournaud@gmail.com>
2014-10-18 16:11:16 +0200

    Added show project view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'projets' => 'projects#index', as: :projects
+  get 'projets/fabre' => 'projects#fabre', as: :fabre
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
