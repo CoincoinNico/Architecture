7cbe331b4cb43e7148bbbe9941e85a487c20d31e
Nicolas Tournaud <ntournaud@gmail.com>
2014-11-18 16:50:17 +0100

    deleted false routes for uncompleted projects

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get 'projets/fabre/travaux' => 'projects#fabreBefore', as: :fabreBefore
   get 'projets/rue-de-babylone' => 'projects#rb', as: :rb
   get 'projets/rue-de-babylone/travaux' => 'projects#rbBefore', as: :rbBefore
-  get 'projets/petites-ecuries' => 'projects#pe', as: :pe
-  get 'projets/saint-nom-la-breteche' => 'projects#snln', as: :snlb
-  get 'projets/serignan-du-comtat' => 'projects#sdc', as: :sdc
+  get 'projets/petites-ecuries' => 'projects#index', as: :pe
+  get 'projets/saint-nom-la-breteche' => 'projects#index', as: :snlb
+  get 'projets/serignan-du-comtat' => 'projects#index', as: :sdc
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
